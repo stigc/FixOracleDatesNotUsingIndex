393a768f37f973e73dc96b20edc30c3c414c54d3
stigc <stig@stigc.dk>
2017-06-08 12:45:04 +0200

    1. version

---
+++ FixOracleDatesNotUsingIndex/OracleDateType.cs
@@ -0,0 +1,81 @@
+﻿using NHibernate;
+using NHibernate.SqlTypes;
+using NHibernate.UserTypes;
+using System;
+using System.Data;
+
+namespace FixOracleDatesNotUsingIndex
+{
+    public class OracleDateType : IUserType
+    {
+        public bool IsMutable
+        {
+            get { return false; }
+        }
+
+        public Type ReturnedType
+        {
+            get { return typeof(DateTime); }
+        }
+
+        public SqlType[] SqlTypes
+        {
+            get { return new[] { NHibernateUtil.Date.SqlType }; }
+        }
+
+        public object NullSafeGet(IDataReader rs, string[] names, object owner)
+        {
+            var obj = NHibernateUtil.DateTime.NullSafeGet(rs, names[0]);
+
+            if (obj == null) return null;
+
+            return Convert.ToDateTime(obj);
+        }
+
+        public void NullSafeSet(IDbCommand cmd, object value, int index)
+        {
+            if (value == null)
+            {
+                ((IDataParameter)cmd.Parameters[index]).Value = DBNull.Value;
+            }
+            else
+            {
+                ((IDataParameter)cmd.Parameters[index]).Value = value;
+            }
+        }
+
+        public object DeepCopy(object value)
+        {
+            return value;
+        }
+
+        public object Replace(object original, object target, object owner)
+        {
+            return original;
+        }
+
+        public object Assemble(object cached, object owner)
+        {
+            return cached;
+        }
+
+        public object Disassemble(object value)
+        {
+            return value;
+        }
+
+        public new bool Equals(object x, object y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+
+            if (x == null || y == null) return false;
+
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(object x)
+        {
+            return x == null ? typeof(DateTime).GetHashCode() + 473 : x.GetHashCode();
+        }
+    }
+}
---
+++ FixOracleDatesNotUsingIndex/Testing.cs
@@ -0,0 +1,136 @@
+﻿using FluentAssertions;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Mapping;
+using NHibernate;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace FixOracleDatesNotUsingIndex
+{
+    public class Testing
+    {
+        public static void RepeatAction(Action action)
+        {
+            for (int i = 0; i < 10; i++)
+                action();
+        }
+
+        [Test]
+        public void SpeedTest()
+        {
+            Console.WriteLine("Starting OptimizedEntity");
+            using (var sf = NhibernateConfiguration.CreateSessionFactory())
+            using (var session = sf.OpenSession())
+            {
+                var sw = Stopwatch.StartNew();
+
+                RepeatAction(() =>
+                {
+                    session.QueryOver<OptimizedEntity>()
+                        .Where(x => x.SomeDate == DateTime.Now)
+                        .List();
+                });
+
+                Console.WriteLine("ElapsedMilliseconds: " + sw.ElapsedMilliseconds);
+            }
+
+            Console.WriteLine("Starting SlowEntity");
+            using (var sf = NhibernateConfiguration.CreateSessionFactory())
+            using (var session = sf.OpenSession())
+            {
+                var sw = Stopwatch.StartNew();
+
+                RepeatAction(() =>
+                {
+                    session.QueryOver<SlowEntity>()
+                        .Where(x => x.SomeDate == DateTime.Now)
+                        .List();
+                });
+
+                Console.WriteLine("ElapsedMilliseconds: " + sw.ElapsedMilliseconds);
+            }
+        }
+
+        [Test]
+        public void OracleDateType_WillWork()
+        {
+            using (var sf = NhibernateConfiguration.CreateSessionFactory())
+            using (var session = sf.OpenSession())
+            {
+                var date = new DateTime(2017, 6, 8, 12, 10, 59);
+                var entity = new OptimizedEntity { Id = "123456", SomeDate = date };
+                session.Save(entity);
+                session.Flush();
+                session.Clear();
+
+                session.QueryOver<SlowEntity>()
+                    .Where(x => x.SomeDate == date)
+                    .SingleOrDefault().Id.Should().Be(entity.Id);
+
+                session.Delete(entity);
+                session.Flush();
+                session.Clear();
+
+                session.QueryOver<SlowEntity>()
+                    .Where(x => x.SomeDate == date)
+                    .List().Should().BeEmpty();
+            }
+        }
+    }
+
+    public class NhibernateConfiguration
+    {
+        public static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                .Database(OracleDataClientConfiguration
+                            .Oracle10
+                            .ConnectionString("Data Source=***;User Id=sys;Password=***;DBA Privilege=SYSDBA;")
+                            .UseReflectionOptimizer()
+                            )
+                            .Cache(c => c.UseMinimalPuts())
+                    .Mappings(m => { m.FluentMappings.AddFromAssemblyOf<OptimizedMap>(); })
+                .BuildSessionFactory();
+        }
+    }
+
+    public class OptimizedEntity
+    {
+        public string Id { get; set; }
+        public DateTime SomeDate { get; set; }
+    }
+
+    public class SlowEntity 
+    {
+        public string Id { get; set; }
+        public DateTime SomeDate { get; set; }
+    }
+
+    public class OptimizedMap : ClassMap<OptimizedEntity>
+    {
+        public OptimizedMap()
+        {
+            Schema("some schema");
+            Table("some table");
+            Not.LazyLoad();
+
+            Id(x => x.Id);
+            Map(x => x.SomeDate, "updat").CustomType<OracleDateType>();
+        }
+    }
+
+    public class SlowMap : ClassMap<SlowEntity>
+    {
+        public SlowMap()
+        {
+            Schema("some schema");
+            Table("some table");
+            Not.LazyLoad();
+
+            Id(x => x.Id);
+            Map(x => x.SomeDate, "updat");
+        }
+    }
+}
